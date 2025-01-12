db3423c6c768a4c79df78b2ba4fd6d79d102b24d
renanzao <renannevessilva@gmail.com>
2025-01-12 18:08:25 -0300

    agora o data_collector persiste e publica mensagem a cada 10segundos

--- data_collector.py
+++ data_collector.py
@@ -1,6 +1,13 @@
 import paho.mqtt.client as mqtt
+import time
 
 mqtt_client = mqtt.Client(client_id ='data_collector')
 mqtt_client.connect(host='localhost', port=1883)
-mqtt_client.publish(topic="/temperatura", payload='{35.5}')
 
+
+mqtt_client.loop_start()
+
+
+while True:
+    mqtt_client.publish("/temperatura", "35.5")
+    time.sleep(10)
